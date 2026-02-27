7b4da002c098def1fa4e64fbf9a8cdbf2059cbe5
doraemon2200 <106516578+doraemon2200@users.noreply.github.com>
2026-02-28 00:14:40 +0530

    android-gradle

--- plugins/withAndroidX.js
+++ plugins/withAndroidX.js
@@ -2,14 +2,17 @@ const { withGradleProperties } = require('@expo/config-plugins');
 
 module.exports = function withAndroidX(config) {
   return withGradleProperties(config, (config) => {
-    config.modResults = config.modResults.filter(
-      (item) => item.key !== 'android.useAndroidX' && 
-                item.key !== 'android.enableJetifier'
+    const properties = config.modResults;
+    
+    // Remove existing entries if present
+    config.modResults = properties.filter(
+      (p) => p.key !== 'android.useAndroidX' && p.key !== 'android.enableJetifier'
     );
-    config.modResults.push(
-      { type: 'property', key: 'android.useAndroidX', value: 'true' },
-      { type: 'property', key: 'android.enableJetifier', value: 'true' }
-    );
+    
+    // Add the required properties
+    config.modResults.push({ type: 'property', key: 'android.useAndroidX', value: 'true' });
+    config.modResults.push({ type: 'property', key: 'android.enableJetifier', value: 'true' });
+    
     return config;
   });
 };
